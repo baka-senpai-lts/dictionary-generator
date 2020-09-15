e9cb15f51ab5dd2e28afac71b365059210ff9f53
baka-senpai <baka.senpai.lts@gmail.com>
2020-09-15 15:24:52 +0300

    Added structure for input data

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Creator: baka@senpai
+ *
+ * You can find me here: https://github.com/baka-senpai-lts
+ *
+ */
+
+#include <iostream>
+
+// Pls, don't beat me :3
+using namespace std;
+
+struct input_data{
+    string name;
+    int age;
+};
+
+int main(int argc, char* argv[]){
+    return 0;
+}
