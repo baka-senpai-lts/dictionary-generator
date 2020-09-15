7679548e9318cae4abb1e0ee4fd2575bf2d61bc0
baka-senpai <baka.senpai.lts@gmail.com>
2020-09-15 15:34:36 +0300

    Added some elements to input_data and added function generate

--- main.cpp
+++ main.cpp
@@ -12,9 +12,32 @@ using namespace std;
 
 struct input_data{
     string name;
-    int age;
+    string age;
+    string birth;
+    string pet;
 };
 
+// This function separates passwords with \n
+
+string generate(input_data data){
+    
+    string result;
+
+    return result;
+
+}
+
 int main(int argc, char* argv[]){
+    
+    input_data data;
+
+    cin >> data.name;
+    cin >> data.age;
+    cin >> data.birth;
+    cin >> data.pet;
+
+    cout << generate(data) << endl;
+    
     return 0;
+
 }
